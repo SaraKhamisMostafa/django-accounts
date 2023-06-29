237b44ee6ac3b70917f6e5697a83837c8e12c4ba
SaraKhamisMostafa <sarakhamismostafa@gmail.com>
2023-06-29 14:50:12 +0300

    add login with email or password

---
+++ accounts/backends.py
@@ -0,0 +1,31 @@
+
+from typing import Any, Optional
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.http.request import HttpRequest
+
+
+
+class EmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user=User.objects.get(
+                Q(username__iexact=username)|
+                Q(email__iexact=username))
+        except User.DoesNotExist:
+            return None
+        except User.MultipleObjectsReturned:
+            return User.objects.filter(email=username).order_by('id').first()    
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
+            
+
+    def get_user(self,user_id):
+        try:
+            user=User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+        return user if self.user_can_authenticate(user) else None   
--- project/settings.py
+++ project/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+LOGIN_REDIRECT_URL='/accounts/profile'
+AUTHENTICATION_BACKENDS =['accounts.backends.EmailBackend']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

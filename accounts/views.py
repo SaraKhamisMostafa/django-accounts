c3e7639cbffaff6810bc36e5ea26788bbdab6129
SaraKhamisMostafa <sarakhamismostafa@gmail.com>
2023-06-29 14:09:43 +0300

    add signup , profile edit

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
-
+from django.shortcuts import render ,redirect
+from .models import Profile
+from .forms import SignupForm ,UserForm ,ProfileForm
+from django.contrib.auth import authenticate , login
 # Create your views here.
+
+
+def signup(request):
+    if request.method=='POST':
+        form=SignupForm(request.POST)
+        if form.is_valid():
+           form.save() 
+           username=form.cleaned_data['username']
+           password=form.cleaned_data['password1']
+           user=authenticate(username=username ,pasword=password)
+           login(request,user)
+           return redirect('/accounts/profile')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html',{'form':form})
+
+
+def profile(request):
+    profile = Profile.objects.get(user=request.user)
+    return render(request,'profile/profile.html',{'profile':profile})
+
+def profile_edit(request):
+     profile=Profile.objects.get(user=request.user)
+     if request.method =='POST':
+         userform=UserForm(request.POST, instance=request.user)
+         profile_form=ProfileForm(request.POST,  instance=profile)
+         if userform.is_valid() and profile_form.is_valid():
+            userform.save()
+            myform =profile_form.save(commit=False)
+            myform.user=request.user
+            myform.save()
+            return redirect('/accounts/profile')
+     else:
+         userform=UserForm(instance=request.user)
+         profile_form=ProfileForm( instance=profile)
+     return render(request,'profile/profile_edit.html',{
+         'userform':userform,
+         'profileform':profile_form
+     })
--- project/urls.py
+++ project/urls.py
@@ -20,5 +20,6 @@ from django.urls import path , include
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
+    path("accounts/", include('accounts.urls',namespace='accounts')),
     
 ]
